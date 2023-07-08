192f99e37d3f0a4788e0dde3c3033a0305cb42f0
Oscar Alvarado <OscarAlvarado@ciencias.unam.mx>
2023-07-08 02:47:53 -0600

    Comandos de la linea de comandos desde Julia

--- prueba.jl
+++ prueba.jl
@@ -1,3 +1,20 @@
-using Plots
+# Set the GPIO 
+# gpio79 is pin 12 on the Jetson Nano Sysfs
+setˍcommand = pipeline(`echo 79`, stdout="/sys/class/gpio/export")
 
-plot([1,2,3,4])
+# Set if the pin is an Output or Input pin
+setupˍcommand = pipeline(`echo out`, stdout="/sys/class/gpio/gpio79/direction")
+
+# Set the digital pin 1 (HIGH) or 0 (LOW)
+onˍcommand = pipeline(`echo 1`, stdout="/sys/class/gpio/gpio79/value")
+offˍcommand = pipeline(`echo 0`, stdout="/sys/class/gpio/gpio79/value")
+#run(setˍcommand)
+#run(setupˍcommand)
+
+for i in 1:100
+	println(i)
+	run(onˍcommand)
+	sleep(0.05)
+	run(offˍcommand)
+	sleep(0.05)
+end
