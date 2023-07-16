c415970b80287ddbb95ac0d44f3c09e94603dd60
oscar.alvarado <oscar.alvarado@credifintech.mx>
2023-07-16 05:34:23 -0600

    Primer intento de la cámara

--- Examples/Camera/take_snapshot.jl
+++ Examples/Camera/take_snapshot.jl
@@ -25,9 +25,7 @@ function gstreamer_pipeline(
     framerate::Int=30;
     flip_method::Int=0
 )::String
-    return """
-    nvarguscamerasrc sensor-id=$sensor_id ! video/x-raw(memory:NVMM)
-    """
+    return `nvarguscamerasrc sensor-id=$sensor_id ! video/x-raw(memory:NVMM)`
 end
 #, width=(int)$capture_width, height=(int)$capture_height, framerate=(fraction)$framerate/1 ! nvvidconv flip-method=$flip_method ! video/x-raw, width=(int)$display_width, height=(int)$display_height, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink
 function show_camera()
