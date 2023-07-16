aef3691e51c01136cfaa50b2cfc870d670b25706
oscar.alvarado <oscar.alvarado@credifintech.mx>
2023-07-15 22:21:38 -0600

    Para hacer la limpieza de los puertos

--- examples/limpieza_puertos.jl
+++ examples/limpieza_puertos.jl
@@ -7,7 +7,7 @@ path = "/sys/class/gpio/"
 directories = filter(isdir, joinpath.(path, readdir(path)))
 
 for directory in directories
-    if "gpio" in directory
+    if ocursin("gpio", basename(directory))
        GPIO.cleanup(parse(Int64, directory[5:end]))
     end
 end
