b6e45cf02bd28ac6c0736bae017f4a6996ffb237
oscar.alvarado <oscar.alvarado@credifintech.mx>
2023-07-15 22:14:45 -0600

    Para hacer la limpieza de los puertos

--- examples/limpieza_puertos.jl
+++ examples/limpieza_puertos.jl
@@ -1,6 +1,7 @@
 include("../src/gpio.jl")
 using .GPIO
 using Base.Filesystem
+import Base.Regex: match
 
 GPIO.setmode()
 path = "/sys/class/gpio/"
@@ -10,7 +11,7 @@ println(directories)
 
 for directory in directories
     print(directory)
-    match = match(r"\d+", directory)
+    match = match(r"\d+", basename(directory))
     println(match.match)
     if match !== nothing
         pintln("Limpiamos puerto $(match.match)")
