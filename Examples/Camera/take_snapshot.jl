7d040c99e48da37fbe2eb9b6e6b30100d6e823c9
oscar.alvarado <oscar.alvarado@credifintech.mx>
2023-07-16 05:23:34 -0600

    Primer intento de la cámara

--- Examples/Camera/take_snapshot.jl
+++ Examples/Camera/take_snapshot.jl
@@ -33,7 +33,7 @@ end
 function show_camera()
     pipeline = gstreamer_pipeline(flip_method=0)
     command = "gst-launch-1.0 "*pipeline
-    run(`gst-launch-1.0 nvarguscamerasrc sensor-id=0`)
+    run(`gst-launch-1.0 nvarguscamerasrc sensor_id=0 !    'video/x-raw(memory:NVMM),width=1920, height=1080, framerate=30/1' !    nvvidconv flip-method=0 ! 'video/x-raw,width=960, height=540' !    nvvidconv ! nvegltransform ! nveglglessink -e`)
 end
 
 # function show_camera()
