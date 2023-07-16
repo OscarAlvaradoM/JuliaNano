963fd08cf2dc2c6d9befb0294a055823ec56620a
oscar.alvarado <oscar.alvarado@credifintech.mx>
2023-07-15 22:19:48 -0600

    Para hacer la limpieza de los puertos

--- examples/limpieza_puertos.jl
+++ examples/limpieza_puertos.jl
@@ -5,11 +5,9 @@ using Base.Filesystem
 GPIO.setmode()
 path = "/sys/class/gpio/"
 directories = filter(isdir, joinpath.(path, readdir(path)))
-println(readdir(path))
-println(directories)
 
 for directory in directories
-    print(basename(directory))
-    
-    #GPIO.cleanup(parse(Int64, match.match))
+    if "gpio" in directory
+       GPIO.cleanup(parse(Int64, directory[5:end]))
+    end
 end
