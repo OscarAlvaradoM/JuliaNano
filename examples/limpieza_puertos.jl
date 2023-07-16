4ac75a43d8cd92ceb0507f89f6af520f29271d44
oscar.alvarado <oscar.alvarado@credifintech.mx>
2023-07-15 21:56:19 -0600

    Para hacer la limpieza de los puertos

--- examples/limpieza_puertos.jl
+++ examples/limpieza_puertos.jl
@@ -9,7 +9,9 @@ directories = filter(isdir, readdir(path))
 numbers = Int64[]
 
 for directory in directories
+    print(directory)
     match = match(r"\d+", directory)
+    println(match.match)
     if match !== nothing
         pintln("Limpiamos puerto $(match.match)")
         GPIO.cleanup(parse(Int64, match.match))
