8464b59ff9747e4f4a106264c677285eb063ef71
oscar.alvarado <oscar.alvarado@credifintech.mx>
2023-07-23 00:36:20 -0600

    Se hacen acomodos para el manejo de fotos

--- Examples/Camera/take_snapshot.jl
+++ Examples/Camera/take_snapshot.jl
@@ -1,7 +1,7 @@
 using Images, FileIO
 
 function takesnapshot(name::String="nvcamtest"; return_image::Bool=false)
-    run(`nvgstcapture-1.0 --automate --capture-auto --start-time=0.5 --file-name=$name`)
+    run(`nvgstcapture-1.0 --automate --capture-auto --start-time=0 --file-name=$name`)
     if return_image
         if occursin("/", name)
             return load(name)
