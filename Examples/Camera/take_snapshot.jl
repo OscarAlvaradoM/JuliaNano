85c97295209b0c15db98aec86448fa1dff1bfa75
oscar.alvarado <oscar.alvarado@credifintech.mx>
2023-07-23 01:12:22 -0600

    Se hacen acomodos para el manejo de fotos

--- Examples/Camera/take_snapshot.jl
+++ Examples/Camera/take_snapshot.jl
@@ -3,12 +3,18 @@ using Images, FileIO
 function takesnapshot(name::String="nvcamtest"; return_image::Bool=false)
     run(`nvgstcapture-1.0 --automate --capture-auto --start-time=0 --file-name=$name`)
     if return_image
-        if occursin("/", name)
-            return load(name)
-        else
-            return load(joinpath(pwd(), name))
-        end
+        path_img = getimagepath(name)
+        return load(path_img)
     end
+end
+
+function getimagepath(name::String)::String
+    semi_path_img = occursin("/", name) ? name : joinpath(pwd(), name)
+    semi_path = join(split(semi_path_img, "/")[1:(end-1)], "/")
+    files_in_dir = readdir(semi_path, join=true)
+    path_img = files_in_dir[argmax(mtime.(files_in_dir))]
+    
+    return path_img
 end
 
 # Example usage
