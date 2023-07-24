4b0067967b1059db2efb8695f886b7126a4b381d
oscar.alvarado <oscar.alvarado@credifintech.mx>
2023-07-23 21:34:47 -0600

    Arreglando lo de la escritura del pwm

--- src/gpio.jl
+++ src/gpio.jl
@@ -423,7 +423,7 @@ module GPIO
         let f_duty_cycle = open(getpwmdutycyclepath(pwm), "r+")
             seek(f_duty_cycle, 0)
             write(f_duty_cycle, string(duty_cycle_ns))
-            close(f_duty_cycle)
+            #close(f_duty_cycle)
             #flush(f_duty_cycle)
         #end
         end
